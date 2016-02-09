79f902a1167224b227e5da20a98835cb94b72c98
John Trapp <johneyt54@gmail.com>
2016-02-09 14:43:38 -0700

    Barebones
    Created a password class and updated main file.

--- CSCI476_Lab2/src/csci476_lab2/CSCI476_Lab2.java
+++ CSCI476_Lab2/src/csci476_lab2/CSCI476_Lab2.java
@@ -1,21 +1,43 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This code is written by John Trapp and Brendan Bellows
+ * for the express purpose to be used in CSCI476 at Montana State University
+ * Spring 2016. Please do not use elseware.
  */
 package csci476_lab2;
 
+import java.util.ArrayList;
+
 /**
  *
- * @author John
+ * @author John Trapp and Brendan Bellows
  */
 public class CSCI476_Lab2 {
+
+    public ArrayList<Password> hashList;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic
+        importHashValues();
+
+        //start timer
+        decodeAll();
+
+        //stop timer
+        printResults();
     }
-    
+
+    private static void importHashValues() {
+        //@todo code to import the hash values into hashList
+    }
+
+    private static void decodeAll() {
+        //@todo code to decode the arrayList hashList
+    }
+
+    private static void printResults() {
+        //@todo prints all of the necessary info to fufill lab requirements
+    }
 }
---
+++ CSCI476_Lab2/src/csci476_lab2/Password.java
@@ -0,0 +1,52 @@
+/*
+ * This code is written by John Trapp and Brendan Bellows
+ * for the express purpose to be used in CSCI476 at Montana State University
+ * Spring 2016. Please do not use elseware.
+ */
+package csci476_lab2;
+
+/**
+ *
+ * @author John Trapp and Brendan Bellows
+ */
+public class Password {
+
+    private String hashValue, decodedPass;
+    private double decodeTime;
+
+    public Password(String hashValueInput) {
+        hashValue = hashValueInput;
+    }
+
+    public String getHashValue() {
+        return hashValue;
+    }
+
+    public String getDecodedPass() {
+        return decodedPass;
+    }
+
+    public double getDecodeTime() {
+        return decodeTime;
+    }
+
+    public void setHashValue(String hashValueInput) {
+        hashValue = hashValueInput;
+    }
+
+    public void setDecodedPass(String decodedPassInput) {
+        decodedPass = decodedPassInput;
+    }
+
+    public void setDecodeTime(double decodeTimeInput) {
+        decodeTime = decodeTimeInput;
+    }
+
+    @Override
+    public String toString() {
+        String output = "The password for hash value " + hashValue + " is "
+                + decodedPass + ", it took the program " + decodeTime
+                + "seconds to recover this password";
+        return output;
+    }
+}
